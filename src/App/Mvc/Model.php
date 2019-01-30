9b2f1abe1fd25b798054d8d2429f753c470305ca
Gleider Ribeiro Costa <gleiderr@gmail.com>
2019-01-30 08:45:29 -0200

    Exibição da tabela Caldas Branco compilada;

--- src/App/Mvc/Model.php
+++ src/App/Mvc/Model.php
@@ -9,19 +9,26 @@ class Model
 
     public function getTypedTable()
     {
-        return [
-            ['traço' => [1.0, 1.0, 2.0], 'cimento(ℓ)' => 363, 'água' => 226],
-            ['traço' => [1.0, 1.5, 3.0], 'cimento(ℓ)' => 273, 'água' => 189],
-            ['traço' => [1.0, 2.0, 2.5], 'cimento(ℓ)' => 264, 'água' => 206],
-            ['traço' => [1.0, 2.0, 3.0], 'cimento(ℓ)' => 243, 'água' => 210],
-            ['traço' => [1.0, 2.5, 3.0], 'cimento(ℓ)' => 225, 'água' => 207],
-            ['traço' => [1.0, 2.0, 4.0], 'cimento(ℓ)' => 210, 'água' => 202],
-            ['traço' => [1.0, 2.5, 3.5], 'cimento(ℓ)' => 207, 'água' => 208],
-            ['traço' => [1.0, 2.5, 4.0], 'cimento(ℓ)' => 195, 'água' => 201],
-            ['traço' => [1.0, 2.5, 5.0], 'cimento(ℓ)' => 174, 'água' => 195],
-            ['traço' => [1.0, 3.0, 5.0], 'cimento(ℓ)' => 162, 'água' => 202],
-            ['traço' => [1.0, 3.0, 6.0], 'cimento(ℓ)' => 147, 'água' => 198],
-            ['traço' => [1.0, 4.0, 8.0], 'cimento(ℓ)' => 114, 'água' => 194],
+        $table = [
+            ['traço' => [1.0, 1.0, 2.0], 'cimento' => 363, 'água' => 226],
+            ['traço' => [1.0, 1.5, 3.0], 'cimento' => 273, 'água' => 189],
+            ['traço' => [1.0, 2.0, 2.5], 'cimento' => 264, 'água' => 206],
+            ['traço' => [1.0, 2.0, 3.0], 'cimento' => 243, 'água' => 210],
+            ['traço' => [1.0, 2.5, 3.0], 'cimento' => 225, 'água' => 207],
+            ['traço' => [1.0, 2.0, 4.0], 'cimento' => 210, 'água' => 202],
+            ['traço' => [1.0, 2.5, 3.5], 'cimento' => 207, 'água' => 208],
+            ['traço' => [1.0, 2.5, 4.0], 'cimento' => 195, 'água' => 201],
+            ['traço' => [1.0, 2.5, 5.0], 'cimento' => 174, 'água' => 195],
+            ['traço' => [1.0, 3.0, 5.0], 'cimento' => 162, 'água' => 202],
+            ['traço' => [1.0, 3.0, 6.0], 'cimento' => 147, 'água' => 198],
+            ['traço' => [1.0, 4.0, 8.0], 'cimento' => 114, 'água' => 194],
         ];
+
+        for ($i=0; $i < count($table); $i++) { 
+            $table[$i]['areia'] = $table[$i]['traço'][1] * $table[$i]['cimento'];
+            $table[$i]['brita'] = $table[$i]['traço'][2] * $table[$i]['cimento'];
+        }
+
+        return $table;
     }
 }
--- src/App/Mvc/View.php
+++ src/App/Mvc/View.php
@@ -1,16 +1,27 @@
 <?php  
 namespace App\Mvc;
+use Twig\Loader;
+
 class View
 {
+    function __construct()
+    {
+        $loader = new \Twig_Loader_Filesystem('./src/App/Mvc/Templates');
+        $this->twig = new \Twig_Environment($loader);
+    }
 
     public function showTypedTable($typedTable)
     {
-        print_r($typedTable);
+        echo "<h1>Tabela Compilada</h1>";
+        echo "<strong>Publicada 29/01/2019</strong>";
+        $template = $this->twig->load('caldas_branco_table.html');
+        echo $template->render(['rows' => $typedTable]);
     }
 
     public function showOriginalTable()
     {
         echo "<h1>Tabela Original</h1>";
         echo "<strong>Publicada 28/01/2019</strong>";
+        echo '<img src="./tabela_caldas_branco.jpg" alt="Tabela Original Caldas Branco">';
     }
 }
